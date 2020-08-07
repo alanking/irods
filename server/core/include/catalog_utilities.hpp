d2553b9d5df0c3b9cbd689f4cd673653010a66df
Alan King <alanking@renci.org>
2020-08-07 15:13:13 -0400

    [XXXXX] Add utility to bind nanodbc statements
    Adds a catalog utility to bind values to nanodbc statements. Also
    adds a mapping between r_data_main columns and said catalog utilities
    so that updating rows in the table becomes simpler.
    
    Notably missing from the columns are the unused columns:
        data_resc_group
        data_resc_name
        data_resc_hier

--- server/core/include/catalog_utilities.hpp
+++ server/core/include/catalog_utilities.hpp
@@ -1,14 +1,59 @@
 #ifndef IRODS_CATALOG_UTILITIES_HPP
 #define IRODS_CATALOG_UTILITIES_HPP
 
+#include "irods_exception.hpp"
 #include "rcConnect.h"
 
+#include "json.hpp"
 #include "nanodbc/nanodbc.h"
 
+#include <functional>
 #include <map>
 #include <string>
+#include <variant>
 
 namespace irods::experimental::catalog {
+    using bind_type = std::variant<std::string, std::uint64_t, int>;
+
+    struct bind_parameters {
+        nanodbc::statement& statement;
+        const std::size_t index;
+        const nlohmann::json& json_input;
+        std::string_view column_name;
+        std::vector<bind_type>& bind_values;
+    };
+
+    using mapping_operator_type = std::function<void(bind_parameters&)>;
+    using column_mapping_operator_type = std::map<std::string, mapping_operator_type>;
+
+    auto bind_string_to_statement(bind_parameters& _bp) -> void;
+    auto bind_bigint_to_statement(bind_parameters& _bp) -> void;
+    auto bind_integer_to_statement(bind_parameters& _bp) -> void;
+
+    namespace data_objects {
+        const column_mapping_operator_type column_mapping_operators{
+            {"data_id",         bind_bigint_to_statement},
+            {"coll_id",         bind_bigint_to_statement},
+            {"data_name",       bind_string_to_statement},
+            {"data_repl_num",   bind_integer_to_statement},
+            {"data_version",    bind_string_to_statement},
+            {"data_type_name",  bind_string_to_statement},
+            {"data_size",       bind_bigint_to_statement},
+            {"data_path",       bind_string_to_statement},
+            {"data_owner_name", bind_string_to_statement},
+            {"data_owner_zone", bind_string_to_statement},
+            {"data_is_dirty",   bind_integer_to_statement},
+            {"data_status",     bind_string_to_statement},
+            {"data_checksum",   bind_string_to_statement},
+            {"data_expiry_ts",  bind_string_to_statement},
+            {"data_map_id",     bind_bigint_to_statement},
+            {"data_mode",       bind_string_to_statement},
+            {"r_comment",       bind_string_to_statement},
+            {"create_ts",       bind_string_to_statement},
+            {"modify_ts",       bind_string_to_statement},
+            {"resc_id",         bind_bigint_to_statement}
+        };
+    } // namespace data_objects
 
     enum class entity_type {
         data_object,
--- server/core/src/catalog_utilities.cpp
+++ server/core/src/catalog_utilities.cpp
@@ -2,7 +2,44 @@
 #include "irods_logger.hpp"
 #include "irods_rs_comm_query.hpp"
 
+namespace {
+    using log = irods::experimental::log;
+} // anonymous namespace
+
 namespace irods::experimental::catalog {
+
+    auto bind_string_to_statement(bind_parameters& _bp) -> void
+    {
+        const auto& v = _bp.json_input.at(_bp.column_name.data()).get<std::string>();
+        _bp.bind_values.push_back(v);
+
+        const auto& value = std::get<std::string>(_bp.bind_values.back());
+        log::database::trace("[{}:{}] - binding [{}] to [{}] at [{}]", __FUNCTION__, __LINE__, _bp.column_name, value, _bp.index);
+
+        _bp.statement.bind(_bp.index, value.c_str());
+    } // bind_string_to_statement
+
+    auto bind_bigint_to_statement(bind_parameters& _bp) -> void
+    {
+        const auto v = std::stoul(_bp.json_input.at(_bp.column_name.data()).get<std::string>());
+        _bp.bind_values.push_back(v);
+
+        const auto value = std::get<std::uint64_t>(_bp.bind_values.back());
+        log::database::trace("[{}:{}] - binding [{}] to [{}] at [{}]", __FUNCTION__, __LINE__, _bp.column_name, value, _bp.index);
+
+        _bp.statement.bind(_bp.index, &value);
+    } // bind_bigint_to_statement
+
+    auto bind_integer_to_statement(bind_parameters& _bp) -> void
+    {
+        const auto v = std::stoi(_bp.json_input.at(_bp.column_name.data()).get<std::string>());
+        _bp.bind_values.push_back(v);
+
+        const auto value = std::get<int>(_bp.bind_values.back());
+        log::database::trace("[{}:{}] - binding [{}] to [{}] at [{}]", __FUNCTION__, __LINE__, _bp.column_name, value, _bp.index);
+
+        _bp.statement.bind(_bp.index, &value);
+    } // bind_integer_to_statement
 
     auto user_has_permission_to_modify_metadata(rsComm_t& _comm,
                                                 nanodbc::connection& _db_conn,
