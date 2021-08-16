6f9f9e8781edabb7fb9c7e4cede502afaef77c4e
Alan King <alanking@renci.org>
2021-08-16 13:22:33 -0400

    squash with f056efc7d4dfa3a6dd90da630587dde8f2649dd6

--- server/core/src/server_utilities.cpp
+++ server/core/src/server_utilities.cpp
@@ -50,7 +50,7 @@ namespace irods
         const auto buf_size = _s.length() + 1;
 
         auto* buf = static_cast<char*>(allocate(sizeof(char) * buf_size));
-        std::strncpy(buf, _s.data(), _s.length());
+        std::snprintf(buf, _s.length(), "%s", _s.data());
 
         auto* bbp = static_cast<BytesBuf*>(allocate(sizeof(BytesBuf)));
         bbp->len = buf_size;
