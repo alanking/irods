5a596f8390a203c062e786437194a6f8263730cc
Alan King <alanking@renci.org>
2022-12-19 16:04:00 -0500

    [ 6428] Use service account for rsModColl redirect
    When redirecting to the catalog service provider in rsModColl, users
    which have been temporarily granted privileged access in the agent
    connection will lose the privileged status on the server-to-server
    connection made there.
    
    This change establishes a temporary connection as the rodsadmin
    authenticated in the local service account so that the privileges remain
    in tact when modifying the collection. This is important so that
    collection mtimes can be updated after an operation is completed even if
    the user does not have any permissions on the parent collection.
    
    NOTE: This may have performance implications, but is required for
    updating collection mtimes to operate completely correctly.

--- server/api/src/rsModColl.cpp
+++ server/api/src/rsModColl.cpp
@@ -1,142 +1,158 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-/* modColl.c
- */
-
-#include "irods/rsModColl.hpp"
+#include "irods/catalog_utilities.hpp"
+#include "irods/client_connection.hpp"
+#include "irods/icatHighLevelRoutines.hpp"
+#include "irods/irods_configuration_keywords.hpp"
+#include "irods/irods_rs_comm_query.hpp"
+#include "irods/miscServerFunct.hpp"
 #include "irods/modColl.h"
 #include "irods/rcMisc.h"
-#include "irods/icatHighLevelRoutines.hpp"
-#include "irods/miscServerFunct.hpp"
-#include "irods/irods_configuration_keywords.hpp"
+#include "irods/rsModColl.hpp"
 
-int
-rsModColl( rsComm_t *rsComm, collInp_t *modCollInp ) {
-    int status;
-    rodsServerHost_t *rodsServerHost = NULL;
-
-    status = getAndConnRcatHost(rsComm, PRIMARY_RCAT, (const char*) modCollInp->collName, &rodsServerHost);
-    if ( status < 0 || NULL == rodsServerHost ) { // JMC cppcheck - nullptr
-        return status;
-    }
-    if ( rodsServerHost->localFlag == LOCAL_HOST ) {
+namespace
+{
+    int _rsModColl(rsComm_t* rsComm, collInp_t* modCollInp)
+    {
         std::string svc_role;
         irods::error ret = get_catalog_service_role(svc_role);
-        if(!ret.ok()) {
+        if (!ret.ok()) {
             irods::log(PASS(ret));
             return ret.code();
         }
 
-        if( irods::KW_CFG_SERVICE_ROLE_PROVIDER == svc_role ) {
-            status = _rsModColl( rsComm, modCollInp );
-        } else if( irods::KW_CFG_SERVICE_ROLE_CONSUMER == svc_role ) {
-            status = SYS_NO_RCAT_SERVER_ERR;
-        } else {
-            rodsLog(
-                LOG_ERROR,
-                "role not supported [%s]",
-                svc_role.c_str() );
-            status = SYS_SERVICE_ROLE_NOT_SUPPORTED;
-        }
-    }
-    else {
-        status = rcModColl( rodsServerHost->conn, modCollInp );
-    }
+        if (irods::KW_CFG_SERVICE_ROLE_PROVIDER == svc_role) {
+            int status;
+            collInfo_t collInfo;
+            char* tmpStr;
 
-    return status;
-}
+            int i;
+            ruleExecInfo_t rei2;
 
-int
-_rsModColl( rsComm_t *rsComm, collInp_t *modCollInp ) {
-    std::string svc_role;
-    irods::error ret = get_catalog_service_role(svc_role);
-    if(!ret.ok()) {
-        irods::log(PASS(ret));
-        return ret.code();
-    }
-
-    if( irods::KW_CFG_SERVICE_ROLE_PROVIDER == svc_role ) {
-        int status;
-        collInfo_t collInfo;
-        char *tmpStr;
-
-        int i;
-        ruleExecInfo_t rei2;
-
-        memset( ( char* )&rei2, 0, sizeof( ruleExecInfo_t ) );
-        rei2.rsComm = rsComm;
-        if ( rsComm != NULL ) {
-            rei2.uoic = &rsComm->clientUser;
-            rei2.uoip = &rsComm->proxyUser;
-        }
-
-        memset( &collInfo, 0, sizeof( collInfo ) );
-
-        rstrcpy( collInfo.collName, modCollInp->collName, MAX_NAME_LEN );
-
-        if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_TYPE_KW))) {
-            rstrcpy(collInfo.collType, tmpStr, NAME_LEN);
-        }
-
-        if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_INFO1_KW))) {
-            rstrcpy(collInfo.collInfo1, tmpStr, MAX_NAME_LEN);
-        }
-
-        if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_INFO2_KW))) {
-            rstrcpy(collInfo.collInfo2, tmpStr, MAX_NAME_LEN);
-        }
-
-        if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_MTIME_KW))) {
-            rstrcpy(collInfo.collModify, tmpStr, TIME_LEN);
-        }
-
-        /**  June 1 2009 for pre-post processing rule hooks **/
-        rei2.coi = &collInfo;
-        i =  applyRule( "acPreProcForModifyCollMeta", NULL, &rei2, NO_SAVE_REI );
-        if ( i < 0 ) {
-            if ( rei2.status < 0 ) {
-                i = rei2.status;
+            memset((char*) &rei2, 0, sizeof(ruleExecInfo_t));
+            rei2.rsComm = rsComm;
+            if (rsComm != NULL) {
+                rei2.uoic = &rsComm->clientUser;
+                rei2.uoip = &rsComm->proxyUser;
             }
-            rodsLog( LOG_ERROR,
-                     "rsGeneralAdmin:acPreProcForModifyCollMeta error for %s,stat=%d",
-                     modCollInp->collName, i );
-            return i;
-        }
-        /**  June 1 2009 for pre-post processing rule hooks **/
 
-        status = chlModColl( rsComm, &collInfo );
+            memset(&collInfo, 0, sizeof(collInfo));
 
-        /**  June 1 2009 for pre-post processing rule hooks **/
-        if ( status >= 0 ) {
-            i =  applyRule( "acPostProcForModifyCollMeta", NULL, &rei2, NO_SAVE_REI );
-            if ( i < 0 ) {
-                if ( rei2.status < 0 ) {
+            rstrcpy(collInfo.collName, modCollInp->collName, MAX_NAME_LEN);
+
+            if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_TYPE_KW))) {
+                rstrcpy(collInfo.collType, tmpStr, NAME_LEN);
+            }
+
+            if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_INFO1_KW))) {
+                rstrcpy(collInfo.collInfo1, tmpStr, MAX_NAME_LEN);
+            }
+
+            if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_INFO2_KW))) {
+                rstrcpy(collInfo.collInfo2, tmpStr, MAX_NAME_LEN);
+            }
+
+            if ((tmpStr = getValByKey(&modCollInp->condInput, COLLECTION_MTIME_KW))) {
+                rstrcpy(collInfo.collModify, tmpStr, TIME_LEN);
+            }
+
+            /**  June 1 2009 for pre-post processing rule hooks **/
+            rei2.coi = &collInfo;
+            i = applyRule("acPreProcForModifyCollMeta", NULL, &rei2, NO_SAVE_REI);
+            if (i < 0) {
+                if (rei2.status < 0) {
                     i = rei2.status;
                 }
-                rodsLog( LOG_ERROR,
-                         "rsGeneralAdmin:acPostProcForModifyCollMeta error for %s,stat=%d",
-                         modCollInp->collName, i );
+                rodsLog(
+                    LOG_ERROR,
+                    "rsGeneralAdmin:acPreProcForModifyCollMeta error for %s,stat=%d",
+                    modCollInp->collName,
+                    i);
                 return i;
             }
-        }
-        /**  June 1 2009 for pre-post processing rule hooks **/
+            /**  June 1 2009 for pre-post processing rule hooks **/
 
-        /* XXXX need to commit */
-        if ( status >= 0 ) {
-            status = chlCommit( rsComm );
+            status = chlModColl(rsComm, &collInfo);
+
+            /**  June 1 2009 for pre-post processing rule hooks **/
+            if (status >= 0) {
+                i = applyRule("acPostProcForModifyCollMeta", NULL, &rei2, NO_SAVE_REI);
+                if (i < 0) {
+                    if (rei2.status < 0) {
+                        i = rei2.status;
+                    }
+                    rodsLog(
+                        LOG_ERROR,
+                        "rsGeneralAdmin:acPostProcForModifyCollMeta error for %s,stat=%d",
+                        modCollInp->collName,
+                        i);
+                    return i;
+                }
+            }
+            /**  June 1 2009 for pre-post processing rule hooks **/
+
+            /* XXXX need to commit */
+            if (status >= 0) {
+                status = chlCommit(rsComm);
+            }
+            else {
+                chlRollback(rsComm);
+            }
+
+            return status;
+        }
+        else if (irods::KW_CFG_SERVICE_ROLE_CONSUMER == svc_role) {
+            return SYS_NO_RCAT_SERVER_ERR;
         }
         else {
-            chlRollback( rsComm );
+            rodsLog(LOG_ERROR, "role not supported [%s]", svc_role.c_str());
+            return SYS_SERVICE_ROLE_NOT_SUPPORTED;
+        }
+    } // _rsModColl
+} // anonymous namespace
+
+int rsModColl(rsComm_t* rsComm, collInp_t* modCollInp)
+{
+    using log_api = irods::experimental::log::api;
+    namespace ic = irods::experimental::catalog;
+
+    try {
+        const auto catalog_provider_host = ic::get_catalog_provider_host();
+
+        if (LOCAL_HOST == catalog_provider_host.localFlag) {
+            ic::throw_if_catalog_provider_service_role_is_invalid();
+            return _rsModColl(rsComm, modCollInp);
         }
 
-        return status;
-    } else if( irods::KW_CFG_SERVICE_ROLE_CONSUMER == svc_role ) {
-        return SYS_NO_RCAT_SERVER_ERR;
-    } else {
-        rodsLog(
-            LOG_ERROR,
-            "role not supported [%s]",
-            svc_role.c_str() );
-        return SYS_SERVICE_ROLE_NOT_SUPPORTED;
+        // Some privileged clients may be affected by temporary elevated privileges. The redirect to the catalog service
+        // provider here will cause the temporary privileged status granted to this connection to be lost in the
+        // server-to-server connection. Many operations in iRODS use this API to update the collection mtime after a
+        // data object modification or creation and requires privileged status to modify the collection in the event
+        // that the user does not have permissions on the parent collection. A client connection is made here using the
+        // service account rodsadmin credentials to modify the collection and then is immediately disconnected.
+        if (irods::is_privileged_client(*rsComm)) {
+            auto conn = irods::experimental::client_connection{
+                catalog_provider_host.hostName->name,
+                rsComm->myEnv.rodsPort,
+                rsComm->myEnv.rodsUserName,
+                rsComm->myEnv.rodsZone};
+
+            RcComm& comm = static_cast<RcComm&>(conn);
+
+            return rcModColl(&comm, modCollInp);
+        }
+
+        auto* host = ic::redirect_to_catalog_provider(*rsComm);
+        return rcModColl(host->conn, modCollInp);
     }
-}
+    catch (const irods::exception& e) {
+        log_api::error("[{}:{}] - caught iRODS exception [{}]", __func__, __LINE__, e.client_display_what());
+        return e.code();
+    }
+    catch (const std::exception& e) {
+        log_api::error("[{}:{}] - caught std::exception [{}]", __func__, __LINE__, e.what());
+        return SYS_INTERNAL_ERR;
+    }
+    catch (...) {
+        log_api::error("[{}:{}] - caught unknown error", __func__, __LINE__);
+        return SYS_UNKNOWN_ERROR;
+    }
+} // rsModColl
