2c45f3a821f9d566824acc5ac4954bed55c27bc2
Alan King <alanking@renci.org>
2022-12-19 16:04:00 -0500

    [ 6428] Use service account for rsModColl redirect
    When redirecting to the catalog service provider in rsModColl, users
    which have been temporarily granted privileged access in the agent
    connection will lose the privileged status on the server-to-server
    connection made there.
    
    This change establishes a temporary connection as the rodsadmin
    authenticated in the local service account so that the privileges remain
    in tact when modifying the collection. This is important so that
    collection mtimes can be updated after an operation is completed even if
    the user does not have any permissions on the parent collection.
    
    NOTE: This may have performance implications, but is required for
    updating collection mtimes to operate completely correctly.

--- lib/core/include/irods/client_connection.hpp
+++ lib/core/include/irods/client_connection.hpp
@@ -18,7 +18,9 @@ namespace irods::experimental
     /// authentication occurs.
     ///
     /// \since 4.2.9
-    struct defer_connection {} defer_connection;
+    const inline struct defer_connection
+    {
+    } defer_connection;
 
     /// This move-only class provides a convenient way to connect to and
     /// disconnect from an iRODS server in a safe manner.
