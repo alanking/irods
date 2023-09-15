97978f8f07d64f73f84a47b038c31fa1eb99c016
Alan King <alanking@renci.org>
2023-09-15 18:49:40 +0000

    [ 7098] Add utility to generate random alphanumeric strings
    Adds a function which randomly generates an alphanumeric character and concatenates
    a string which is returned to the user. Unit tests included.

--- lib/core/src/irods_random.cpp
+++ lib/core/src/irods_random.cpp
@@ -1,7 +1,34 @@
-#include <boost/random.hpp>
-#include <openssl/rand.h>
 #include "irods/irods_random.hpp"
+
+#include "irods/irods_exception.hpp"
+#include "irods/rodsErrorTable.h"
+
+#include <boost/random.hpp>
+#include <fmt/format.h>
+#include <openssl/rand.h>
+
 #include <ctime>
+#include <random>
+#include <string>
+
+namespace
+{
+    auto generate_random_alphanumeric_character() -> char
+    {
+        constexpr char min_char = '0';
+        constexpr char max_char = 'z';
+
+        static std::random_device rd;
+        static std::default_random_engine e{rd()};
+        static std::uniform_int_distribution<> d{min_char, max_char};
+
+        char c{};
+        while (!std::isalnum(c)) {
+            c = static_cast<char>(d(e));
+        }
+        return c;
+    } // generate_random_alphanumeric_character
+} // anonymous namespace
 
 void irods::getRandomBytes( void * buf, int bytes ) {
     if ( RAND_bytes( ( unsigned char * )buf, bytes ) != 1 ) {
@@ -13,3 +40,26 @@ void irods::getRandomBytes( void * buf, int bytes ) {
         }
     }
 }
+
+auto irods::generate_random_alphanumeric_string(std::size_t _length) -> std::string
+{
+    try {
+        std::string s;
+        s.reserve(_length);
+
+        for (std::size_t i = 0; i < _length; ++i) {
+            s += generate_random_alphanumeric_character();
+        }
+
+        return s;
+    }
+    catch (const std::length_error& e) {
+        THROW(SYS_INVALID_INPUT_PARAM, fmt::format("{}: Invalid length. [{}]", __func__, e.what()));
+    }
+    catch (const std::exception& e) {
+        THROW(SYS_LIBRARY_ERROR, fmt::format("{}: Failed to generate string. [{}]", __func__, e.what()));
+    }
+    catch (...) {
+        THROW(SYS_UNKNOWN_ERROR, "Failed to generate string: [Unknown error]");
+    }
+} // generate_random_alphanumeric_string
