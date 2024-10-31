1f96cf19257c2de3c88e087a06ff5913e5311a92
Alan King <alanking@renci.org>
2024-10-31 17:07:37 -0400

    [ XXXX] char* -> const char* because we can

--- server/core/include/irods/rodsConnect.h
+++ server/core/include/irods/rodsConnect.h
@@ -96,10 +96,10 @@ int getAndConnRcatHost(rsComm_t *rsComm,
                        const char *rcatZoneHint,
                        rodsServerHost_t **rodsServerHost);
 
-int getAndConnRcatHostNoLogin(rsComm_t *rsComm,
+int getAndConnRcatHostNoLogin(rsComm_t* rsComm,
                               int rcatType,
-                              char *rcatZoneHint,
-                              rodsServerHost_t **rodsServerHost);
+                              const char* rcatZoneHint,
+                              rodsServerHost_t** rodsServerHost);
 
 /// \brief Gets the Catalog Service Provider host information from `ZoneInfoHead`.
 ///
