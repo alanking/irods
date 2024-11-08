defd799c890a1c5dd888a7e751ae58e3508a7eb3
Alan King <alanking@renci.org>
2024-11-08 16:11:18 -0500

    MAke basic plugin again

---
+++ plugins/auth/include/irods/plugins/auth/basic.hpp
@@ -0,0 +1,9 @@
+#ifndef IRODS_PLUGIN_AUTH_BASIC_HPP
+#define IRODS_PLUGIN_AUTH_BASIC_HPP
+
+namespace irods::experimental::auth::scheme
+{
+    constexpr char* basic = "basic";
+} // namespace irods::experimental::auth::scheme
+
+#endif // IRODS_PLUGIN_AUTH_BASIC_HPP
